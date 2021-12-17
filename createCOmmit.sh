e6ffbd5c18b3ad62cacaff7ad3239831c98858c9
root <root>
2021-12-17 15:21:15 +0530

    SS

--- createCOmmit.sh
+++ createCOmmit.sh
@@ -1,12 +1,11 @@
 string=""
 x=1
 j=1
-while [ $x -le 100 ]
+while [ $x -le 200 ]
 do
  string="touch randomfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
  echo $string
  eval "$string"
  x=$[$x+1]
-#sleep 5.0
 done
 git push
